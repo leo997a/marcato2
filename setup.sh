2826489d1e4fb29fff3f25b5d74ad615b5f10bb4
leo997a <166352340+leo997a@users.noreply.github.com>
2025-05-27 21:27:12 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,10 @@
 # إزالة ذاكرة التخزين المؤقت لـ webdriver_manager
 rm -rf ~/.wdm
 
-# تنزيل ChromeDriver الإصدار 120.0.6099.224 يدويًا
+# تنزيل ChromeDriver الإصدار 120.0.6099.224
 wget https://chromedriver.storage.googleapis.com/120.0.6099.224/chromedriver-linux64.zip
 unzip chromedriver-linux64.zip
+mkdir -p /usr/local/bin
 mv chromedriver-linux64/chromedriver /usr/local/bin/chromedriver
 chmod +x /usr/local/bin/chromedriver
 rm -rf chromedriver-linux64.zip chromedriver-linux64
